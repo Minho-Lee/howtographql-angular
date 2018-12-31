c376199a8da0dcc7b09c578620238c66f503a640
Min Lee <minho.lee.93@hotmail.com>
2018-12-31 01:13:48 -0500

    Add header for navigation and add an update method to createLink mutation in order to update the given data.links and navigate back to 'Links'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LinkListComponent } from './link-list/link-list.component';
+import { CreateLinkComponent } from './create-link/create-link.component';
+
+const routes: Routes = [
+  { path: '', component: LinkListComponent, pathMatch: 'full' },
+  { path: 'create', component: CreateLinkComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/create-link/create-link.component.ts
+++ src/app/create-link/create-link.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 
-import { CREATE_LINK_MUTATION, CreateLinkMutationResponse } from '../graphql';
+import {
+  ALL_LINKS_QUERY,
+  CREATE_LINK_MUTATION,
+  CreateLinkMutationResponse
+} from '../graphql';
 
 @Component({
   selector: 'app-create-link',
@@ -13,7 +18,8 @@ export class CreateLinkComponent implements OnInit {
   url: string;
 
   constructor(
-    public apollo: Apollo
+    public apollo: Apollo,
+    public router: Router
   ) { }
 
   ngOnInit() {
@@ -25,9 +31,21 @@ export class CreateLinkComponent implements OnInit {
       variables: {
         description: this.description,
         url: this.url
-      }})
+      },
+      // store, data are provided from apollo
+      // store is DataProxy where we can keep a local state
+      update: (store, { data: { createLink } }) => {
+        const data: any = store.readQuery({
+          query: ALL_LINKS_QUERY
+        });
+
+        data.links.push(createLink);
+        store.writeQuery({ query: ALL_LINKS_QUERY, data});
+      }
+    })
     .subscribe((response) => {
       console.log('mutation:', response);
+      this.router.navigate(['/']);
     });
 
   }
